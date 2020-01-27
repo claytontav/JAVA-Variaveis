e1621e6d652db5860fdda79149cef0b18867bc11
Clayton Tavares <54451974+claytongit@users.noreply.github.com>
2020-01-27 19:55:11 -0300

    Add files via upload

---
+++ Variaveis/Variaveis.java
@@ -0,0 +1,14 @@
+class Variaveis {
+    public static void main(String[] args){
+
+        String nome = "Clayton";
+        int idade = 26;
+        float peso = 80.0f;
+        boolean dev = true;
+
+        System.out.println("Seu nome é: " + nome);
+        System.out.println("Sua idade é: " + idade);
+        System.out.println("Seu peso é: " + peso);
+        System.out.println("Você é desenvolvedor? " + dev);
+    }
+}
